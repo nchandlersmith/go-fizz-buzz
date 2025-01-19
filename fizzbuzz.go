47d19423222893e912e01d40047dc215a2822f65
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2025-01-19 11:06:49 -0600

    handle fizz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,7 +3,7 @@ package go_fizz_buzz
 import "strconv"
 
 func FizzBuzz(n int) string {
-	if 3 == n {
+	if 0 == n%3 {
 		return "Fizz"
 	}
 	return strconv.Itoa(n)
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -14,6 +14,14 @@ func TestFizzBuzz_returns_Fizz_when_3(t *testing.T) {
 	assertEquals(got, want, t)
 }
 
+func TestFizzBuzz_returns_Fizz_when_divisible_by_3(t *testing.T) {
+	test_cases := []int{3, 6, 9, 12}
+	for _, in := range test_cases {
+		got := FizzBuzz(in)
+		assertEquals(got, "Fizz", t)
+	}
+}
+
 func assertEquals(got string, want string, t *testing.T) {
 	if got != want {
 		t.Errorf("Expected got: %v to equal want: %v", got, want)
